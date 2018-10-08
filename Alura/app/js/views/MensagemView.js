6921641ecc681b39b6834e15b39993e6131b65e6
Paulo Barros <paulo.barros@lambda3.com.br>
2018-10-08 16:30:00 -0300

    Alteracao no gitignore

--- Alura/app/js/views/MensagemView.js
+++
@@ -1,5 +0,0 @@
-class MensagemView extends View {
-    template(modelo) {
-        return `<p class="alert alert-info">${modelo}</p>`;
-    }
-}
--- Alura/app/ts/views/MensagemView.ts
+++
@@ -1,7 +0,0 @@
-class MensagemView extends View<string> {
-
-  template(modelo: string): string {
-    return `<p class="alert alert-info">${modelo}</p>`;
-  }
-
-}
