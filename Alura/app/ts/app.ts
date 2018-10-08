78167b43d961d5d444342713091e944a0f31e9f2
Paulo Barros <paulo.barros@lambda3.com.br>
2018-10-08 17:08:07 -0300

    Aula 05 - Instalando typescript definitions

--- Alura/app/ts/app.ts
+++ Alura/app/ts/app.ts
@@ -1,4 +1,6 @@
 const controller = new NegociacaoController();
 
-document.querySelector('.form')
-  .addEventListener('submit', controller.adicionar.bind(controller));
+// document.querySelector('.form')
+//   .addEventListener('submit', controller.adicionar.bind(controller));
+
+$('.form').submit(controller.adicionar.bind(controller));
--- Alura/app/ts/views/View.ts
+++ Alura/app/ts/views/View.ts
@@ -1,13 +1,13 @@
 abstract class View<T> {
   
-  private _elemento: Element;
+  private _elemento: JQuery;
 
   constructor(seletor: string) {
-    this._elemento = document.querySelector(seletor);
+    this._elemento = $(seletor);
   }
 
   update(modelo: T): void {
-    this._elemento.innerHTML = this.template(modelo);
+    this._elemento.html(this.template(modelo));
   }
 
   abstract template(modelo: T): string;
