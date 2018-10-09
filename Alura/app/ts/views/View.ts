d2378eb40a441da4659672f5ce9fdf8a76480273
Paulo Barros <paulo.barros@lambda3.com.br>
2018-10-09 17:33:22 -0300

    Aula 08 - Decorador de metodo

--- Alura/app/ts/views/View.ts
+++ Alura/app/ts/views/View.ts
@@ -1,3 +1,5 @@
+import { logarTempoDeExecucao } from "../helpers/decorators/index";
+
 export abstract class View<T> {
 
   private _elemento: JQuery;
@@ -8,6 +10,7 @@ export abstract class View<T> {
     this._escape = escape;
   }
 
+  @logarTempoDeExecucao()
   update(modelo: T): void {
     let template = this.template(modelo);
 
